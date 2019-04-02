4531daee1d7865bb338386ac86bcf5030fbd734c
Turzhanskyi <turzhansky81@gmail.com>
2019-04-02 17:04:44 +0300

    Added const&let

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // тут пишем код
+    const apiUrl = 'https://api.exchangeratesapi.io/latest?base=';
+    const defaultBase = 'EUR';
+    const fromBases = ['RUB', 'EUR', 'USD', 'ISK', 'GBP', 'JPY'];
+
+    const fromCurrency = document.getElementById("fromCurrency");
+    const toCurrency = document.getElementById("toCurrency");
+    const calculateButton = document.getElementById("submit");
+    const result = document.getElementById('result');
+    const header = document.getElementById('header');
+    const startUpdate = document.getElementById('start-update');
+    const display = document.getElementById('timer');
+    const currencyIcon = document.getElementById('currencyIcon');
+    const amountInput = document.getElementById('amount');
+    const timerSelector = document.getElementById('how-to-update');
+    const timerIcon = document.getElementById('start-icon');
+    const timerFooter = document.getElementById('timer-footer');
+
+    let currencyRatesData = {};
+    let bases = [];
 
 });
